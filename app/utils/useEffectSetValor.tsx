2e25b771b7b11aed6ebbeeff9d8852ee79ab1361
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-07-04 10:22:36 -0300

    feat: delete setValor and create useEffectSetValor

---
+++ app/utils/useEffectSetValor.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useEffect } from "react";
+import { success } from "../constants/toast";
+
+interface Row {
+  descricao: string;
+  valor: string;
+  tipo: "entrada" | "saida";
+}
+
+export const useEffectSetValor = (setInitial: any) => {
+  useEffect(() => {
+    const add = document.getElementById("add");
+    const descricao = document.getElementById("descricao") as HTMLInputElement;
+    const val = document.getElementById("valor") as HTMLInputElement;
+    const tipos = document.querySelectorAll<HTMLInputElement>(
+      'input[name="group"]'
+    );
+
+    const handleClick = () => {
+      if (descricao.value === "") {
+        alert("Adicione uma Descrição");
+        return;
+      }
+
+      if (val.value === "") {
+        alert("Adicione um Valor");
+        return;
+      }
+
+      let value = "";
+      tipos.forEach((tipo) => {
+        if (tipo.checked) {
+          value = tipo.value;
+        }
+      });
+
+      const newObj: Row = {
+        descricao: descricao.value,
+        valor: val.value,
+        tipo: value as "entrada" | "saida",
+      };
+
+      setInitial((prevRows: any) => [...prevRows, newObj]);
+
+      descricao.value = "";
+      val.value = "";
+
+      success("Adicionado com Sucesso");
+    };
+
+    add?.addEventListener("click", handleClick);
+
+    return () => {
+      add?.removeEventListener("click", handleClick);
+    };
+  }, []);
+};

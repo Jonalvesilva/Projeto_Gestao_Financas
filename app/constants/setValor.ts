8939a281ff43bcb2884cd455fc2072881db7db7c
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-07-04 09:49:55 -0300

    feat: add listener domContentLoad

--- app/constants/setValor.ts
+++ app/constants/setValor.ts
@@ -7,48 +7,50 @@ interface Row {
 }
 
 export function setAddValor(setInitial: any) {
-  const add = document.getElementById("add");
-  const descricao = document.getElementById("descricao") as HTMLInputElement;
-  const val = document.getElementById("valor") as HTMLInputElement;
-  const tipos = document.querySelectorAll<HTMLInputElement>(
-    'input[name="group"]'
-  );
+  document.addEventListener("DOMContentLoaded", () => {
+    const add = document.getElementById("add");
+    const descricao = document.getElementById("descricao") as HTMLInputElement;
+    const val = document.getElementById("valor") as HTMLInputElement;
+    const tipos = document.querySelectorAll<HTMLInputElement>(
+      'input[name="group"]'
+    );
 
-  const handleClick = () => {
-    if (descricao.value === "") {
-      alert("Adicione uma Descrição");
-      return;
-    }
-
-    if (val.value === "") {
-      alert("Adicione um Valor");
-      return;
-    }
-
-    let value = "";
-    tipos.forEach((tipo) => {
-      if (tipo.checked) {
-        value = tipo.value;
+    const handleClick = () => {
+      if (descricao.value === "") {
+        alert("Adicione uma Descrição");
+        return;
       }
-    });
 
-    const newObj: Row = {
-      descricao: descricao.value,
-      valor: val.value,
-      tipo: value as "entrada" | "saida",
+      if (val.value === "") {
+        alert("Adicione um Valor");
+        return;
+      }
+
+      let value = "";
+      tipos.forEach((tipo) => {
+        if (tipo.checked) {
+          value = tipo.value;
+        }
+      });
+
+      const newObj: Row = {
+        descricao: descricao.value,
+        valor: val.value,
+        tipo: value as "entrada" | "saida",
+      };
+
+      setInitial((prevRows: any) => [...prevRows, newObj]);
+
+      descricao.value = "";
+      val.value = "";
+
+      success("Adicionado com Sucesso");
     };
 
-    setInitial((prevRows: any) => [...prevRows, newObj]);
+    add?.addEventListener("click", handleClick);
 
-    descricao.value = "";
-    val.value = "";
-
-    success("Adicionado com Sucesso");
-  };
-
-  add?.addEventListener("click", handleClick);
-
-  return () => {
-    add?.removeEventListener("click", handleClick);
-  };
+    return () => {
+      add?.removeEventListener("click", handleClick);
+    };
+  });
 }
